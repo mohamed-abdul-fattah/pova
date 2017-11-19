e196940cbfe2c1eb4fe31b1348cf078554854789
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2017-11-19 19:42:16 +0200

    styling in en

--- resources/views/frontend/resources/styles.blade.php
+++ resources/views/frontend/resources/styles.blade.php
@@ -1,4 +1,7 @@
 <link rel="stylesheet" href="/css/component.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.2.0/jquery.fancybox.min.css" />
 <link rel="stylesheet" href="/css/resources-fields.css">
+@if (app()->getLocale() === 'ar')
+    <link rel="stylesheet" href="/css/resources-fields-rtl.css">
+@endif
 <link rel="stylesheet" href="/css/jquery-ui.multidatespicker.css">
