db93198f0ede68b43622ef58184ad046f932ea7d
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2017-11-20 19:42:54 +0200

    404 page and footer copyrights

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.frontend.app')
+
+@section('content')
+    <!-- start main-content -->
+    <div class="main-content">
+      <!-- Section: home -->
+      <section id="home" class="fullscreen parallax text-center layer-overlay" data-bg-img="images/bg/bg1.jpg">
+        <div class="display-table text-center">
+          <div class="display-table-cell">
+            <div class="container pt-0 pb-0">
+              <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                  <h1 class="text-gray-silver font-200 line-height-1em mb-0">404</h1>
+                  <h2 class="text-white mt-0">Oops! Page Not Found</h2>
+                  <h2 class="text-white mt-0">خطأ! تعذر الوصول للصفحة</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+    <!-- end main-content -->
+@endsection
